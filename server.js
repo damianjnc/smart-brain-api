b532786cbdd8ccffb0489e816ae7150f053f8fe1
Damian <damianjnc@gmail.com>
2020-01-05 17:53:51 +0000

    updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -15,24 +16,23 @@ dotenv.config()
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    connectionString : process.env.POSTGRES_URI ,
+    ssl: false,
   }
 });
 
 const app = express();
 
+app.use(morgan('combined'))
 app.use(cors())
-
 app.use(bodyParser.json())
 
-app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3000, ()=> {
+  console.log(`app is running on port 3000`);
 })
